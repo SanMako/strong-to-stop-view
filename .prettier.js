a56af1d9bfdeb47f0624825b32310cfd83b9bb6c
Mr.Jin-晋 <11231004.bjtu.edu.cn>
2022-04-16 10:11:42 +0800

    添加代码规范约束prettier,并添加脚本测试

---
+++ .prettier.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80, // 超过最大值换行
+  tabWidth: 2, // 缩进字节数,默认为2
+  useTabs: true, // 缩进不使用tab，使用空格
+  semi: true, // 句尾添加分号
+  singleQuote: false, // 使用单引号代替双引号
+  endOfLine: "lf", // 结尾是 \n \r \n\r auto
+  arrowParens: "always", // (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号，always：总是有括号
+};
